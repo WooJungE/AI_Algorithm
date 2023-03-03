4de5595c7deaef064c9e280ea29caf9eb457ec17
WooJungE <114748876+WooJungE@users.noreply.github.com>
2023-03-03 14:28:17 +0900

    Create AI_7.py

---
+++ AI_7.py
@@ -0,0 +1,54 @@
+# 의사결정 나무(Decision Tree) 알고리즘은 분류(Classification) 및 회귀(Regression) 분석에 사용되는 지도 학습(Supervised Learning) 알고리즘 중 하나
+# 이 알고리즘은 특정 데이터 포인트의 속성 값을 기반으로 다른 속성 값을 예측하는 데 사용
+# JavaScript를 사용하여 의사결정 나무 알고리즘을 구현한 예시 코드
+
+// 의사결정 나무 모델 클래스 정의
+class DecisionTree {
+  constructor(data) {
+    this.data = data;
+    this.tree = this.buildTree(this.data);
+  }
+
+  // 의사결정 나무 모델 구축
+  buildTree(data) {
+    // 트리 구축 알고리즘
+    // ...
+  }
+
+  // 입력된 데이터로 모델 예측
+  predict(input) {
+    // 모델 예측 알고리즘
+    // ...
+  }
+}
+
+// 데이터 셋 생성
+const data = [
+  ['Sunny', 'Hot', 'High', 'Weak', 'No'],
+  ['Sunny', 'Hot', 'High', 'Strong', 'No'],
+  ['Overcast', 'Hot', 'High', 'Weak', 'Yes'],
+  ['Rain', 'Mild', 'High', 'Weak', 'Yes'],
+  ['Rain', 'Cool', 'Normal', 'Weak', 'Yes'],
+  ['Rain', 'Cool', 'Normal', 'Strong', 'No'],
+  ['Overcast', 'Cool', 'Normal', 'Strong', 'Yes'],
+  ['Sunny', 'Mild', 'High', 'Weak', 'No'],
+  ['Sunny', 'Cool', 'Normal', 'Weak', 'Yes'],
+  ['Rain', 'Mild', 'Normal', 'Weak', 'Yes'],
+  ['Sunny', 'Mild', 'Normal', 'Strong', 'Yes'],
+  ['Overcast', 'Mild', 'High', 'Strong', 'Yes'],
+  ['Overcast', 'Hot', 'Normal', 'Weak', 'Yes'],
+  ['Rain', 'Mild', 'High', 'Strong', 'No']
+];
+
+// 의사결정 나무 모델 생성
+const dt = new DecisionTree(data);
+
+// 새로운 데이터로 모델 예측
+const newInput = ['Sunny', 'Hot', 'Normal', 'Weak'];
+const prediction = dt.predict(newInput);
+console.log(prediction);
+
+# DecisionTree 클래스는 데이터셋을 사용하여 의사결정 나무 모델을 구축
+# predict 메소드는 새로운 입력 값을 가져와서 해당 값에 대한 예측 결과를 출력
+# 이 코드는 의사결정 나무 알고리즘의 구현을 이해하기 위한 코드
+     # 실제 데이터 셋에 따라 코드가 달라질 수 있음
