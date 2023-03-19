9323eeb1232742d5c48b05b4f0cdf263faea4454
WooJungE <114748876+WooJungE@users.noreply.github.com>
2023-03-19 18:22:35 +0900

    Update and rename AI_7.py to AI_7.js

--- AI_7.js
+++ AI_7.js
@@ -1,6 +1,6 @@
-# 의사결정 나무(Decision Tree) 알고리즘은 분류(Classification) 및 회귀(Regression) 분석에 사용되는 지도 학습(Supervised Learning) 알고리즘 중 하나
-# 이 알고리즘은 특정 데이터 포인트의 속성 값을 기반으로 다른 속성 값을 예측하는 데 사용
-# JavaScript를 사용하여 의사결정 나무 알고리즘을 구현한 예시 코드
+// 의사결정 나무(Decision Tree) 알고리즘은 분류(Classification) 및 회귀(Regression) 분석에 사용되는 지도 학습(Supervised Learning) 알고리즘 중 하나
+// 이 알고리즘은 특정 데이터 포인트의 속성 값을 기반으로 다른 속성 값을 예측하는 데 사용
+// JavaScript를 사용하여 의사결정 나무 알고리즘을 구현한 예시 코드
 
 // 의사결정 나무 모델 클래스 정의
 class DecisionTree {
@@ -48,7 +48,7 @@ const newInput = ['Sunny', 'Hot', 'Normal', 'Weak'];
 const prediction = dt.predict(newInput);
 console.log(prediction);
 
-# DecisionTree 클래스는 데이터셋을 사용하여 의사결정 나무 모델을 구축
-# predict 메소드는 새로운 입력 값을 가져와서 해당 값에 대한 예측 결과를 출력
-# 이 코드는 의사결정 나무 알고리즘의 구현을 이해하기 위한 코드
-     # 실제 데이터 셋에 따라 코드가 달라질 수 있음
+// DecisionTree 클래스는 데이터셋을 사용하여 의사결정 나무 모델을 구축
+// predict 메소드는 새로운 입력 값을 가져와서 해당 값에 대한 예측 결과를 출력
+// 이 코드는 의사결정 나무 알고리즘의 구현을 이해하기 위한 코드
+     // 실제 데이터 셋에 따라 코드가 달라질 수 있음
