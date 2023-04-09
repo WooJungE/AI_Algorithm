c11340c1ef56b2daa074268d2d93587c16003b75
WooJungE <114748876+WooJungE@users.noreply.github.com>
2023-04-10 00:06:13 +0900

    Update 0410_2.py

--- 0410_2.py
+++ 0410_2.py
@@ -1,3 +1,7 @@
+#2차원 평면상에 n개의 점이 있다. 가장 인접한 쌍의 거리를 구하기.
+#두 점 사이의 거리를 계산할 때는 유클리드 거리(Euclidean distance)를 이용
+#유클리드 거리를 구할 때 math 모듈의 sqrt()와 거듭제곱 연산자 사용
+
 import math
 def distance(p1, p2):
     return math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
