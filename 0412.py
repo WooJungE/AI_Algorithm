bcebd59e839c5d7e11bd493f01c2f029fbd5737f
WooJungE <114748876+WooJungE@users.noreply.github.com>
2023-04-12 21:00:46 +0900

    Create 0412.py

---
+++ 0412.py
@@ -0,0 +1,22 @@
+#정렬 문제를 축소 정복 기법으로 해결한 삽입 정렬 알고리즘
+
+def insertion_sort(A) :
+    n = len(A)
+    for i in range(1, n) :
+        key = A[i]
+        j = i-1
+        while j>=0 and A[j] > key :
+            A[j+1] = A[j]
+            j = j-1
+        A[j+1] = key
+        printStep(A, i)
+
+def printStep(arr, val) :
+    print("  Step %2d = " % val, end='')
+    print(arr)
+
+data = [int(x) for x in input().split()]
+
+print("Original  :", data)
+insertion_sort(data)
+print("Insertion :", data)
