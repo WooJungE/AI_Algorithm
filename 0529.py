375afdf598d64a130e220e00c1c16cea031e43dc
WooJungE <114748876+WooJungE@users.noreply.github.com>
2023-05-29 08:55:22 +0900

    Create 0529.py

---
+++ 0529.py
@@ -0,0 +1,22 @@
+#선형조사법(linear probing)에 의한 오버플로 처리를 하는 삭제 연산을 수행하는 함수 lp_delete(key)를 완성해 보기
+
+M = 13					# 테이블의 크기
+def hashFn(key) :		# 해시 함수
+    return key % M
+
+def lp_delete(key) :
+    id = hashFn(key)
+    count = M
+    while count>0 :
+        if table[id] == None :return
+        if table[id] != -1 and table[id] == key :
+            table[id] = -1
+            return
+        id = (id+1+M) % M
+        count -= 1
+
+table = [None if m == "None" else int(m) for m in input().split()]
+key = int(input())
+
+lp_delete(key)
+print(table)
